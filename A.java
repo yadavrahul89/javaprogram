b01f73e0c2ec976e9e4e7bf506af75c2f6e27b75
yadavrahul89 <rahulyadav808817@gmail.com>
2023-12-15 12:00:44 +0530

    addnew

---
+++ A.java
@@ -0,0 +1,35 @@
+package test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class A {
+	
+	public static void main(String[] args) {
+		
+		List<String> asList = Arrays.asList("a","b","c","d","c");
+		rep(asList);
+	}
+
+	static void rep(List<String> str) {
+		Map<String, Integer> map = new HashMap<>();
+		
+		for(String val : str) {
+//			Integer integer = map.get(val);
+			Integer orDefault = map.getOrDefault(val, 1);
+			
+			if(map.containsKey(val)) {
+				map.put(val, orDefault+1);
+			}else {
+				map.put(val, 1);
+			}
+			
+			
+			map.entrySet().stream().filter(x -> x.getValue()>1).forEach((y->{
+				System.out.println(y.getKey()+" "+y.getValue());
+			}));
+		}
+	}
+}
